705b208cfc8e80f762e71ddb261bc5160b957baf
miki <mikiymk001@gmail.com>
2021-08-21 18:50:02 +0900

    format documents

--- javascript/main.js
+++ javascript/main.js
@@ -4,15 +4,15 @@ const rootid = "div";
  * do this first
  */
 function main() {
- alert("javascript start");
- try {
-  let div = document.getElementById(rootid);
-  let canvas = createCanvasElement();
-  div.appendChild(canvas);
- } catch (error) {
-  alert(error);
- }
- alert("javascript done");
+    alert("javascript start");
+    try {
+        let div = document.getElementById(rootid);
+        let canvas = createCanvasElement();
+        div.appendChild(canvas);
+    } catch (error) {
+        alert(error);
+    }
+    alert("javascript done");
 }
 
 /*
@@ -20,10 +20,10 @@ function main() {
  * @returns canvas
  */
 function createCanvasElement() {
- let element = document.createElement("canvas");
- element.setAttribute("height", 500);
- element.setAttribute("width", 500);
- return element;
+    let element = document.createElement("canvas");
+    element.setAttribute("height", 500);
+    element.setAttribute("width", 500);
+    return element;
 }
 
 main();
