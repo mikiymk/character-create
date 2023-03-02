5642989195d72477049f5f35163e77803ea06c71
ymk <65121726+mikiymk@users.noreply.github.com>
2023-03-02 19:42:59 +0900

    要素を作る関数

--- javascript/main.js
+++ javascript/main.js
@@ -11,8 +11,30 @@ export function main(root) {
  * @returns canvas
  */
 function createCanvasElement() {
-  let element = document.createElement("canvas");
-  element.setAttribute("height", 500);
-  element.setAttribute("width", 500);
+  return h("canvas", { height: 500, width: 500 });
+}
+
+/*
+ * タグ名、属性、子要素でタグ要素を作ります。
+ * @param tag {string} タグ名
+ */
+function h(tag, attrs, children) {
+  let element = document.createElement(tag);
+
+  for (let attr in attrs || {}) {
+    element.setAttribute(attr, attrs[attr]);
+  }
+
+  for (let child of children || []) {
+    element.appendChild(child);
+  }
+
   return element;
+}
+
+/*
+ * テキスト要素を作ります。
+ */
+function t(text) {
+  return document.createTextNode(text);
 }
